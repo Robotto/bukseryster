203d890ae27a7b823bf753a04fdda14c05a72239
robotto <mark@moore.dk>
2015-09-08 13:50:45 +0200

    A countinuous connection is now maintained between robottobox and the bukseryster hardware

--- client/src/bukseryster.ino
+++ client/src/bukseryster.ino
@@ -6,10 +6,25 @@ enum fonaStates {
 	off,
 	on,
 	connected,
-	gprsActive
+	gprsActive,
+	TCPconnected
 };
 
+
+
 //APN settings for /some/ danish service providers: http://www.appsandroid.dk/apn-indstillinger-mmsgprs.html
+
+//TO USE DNS:
+/*
+// Connect with domain name server
+AT+CDNSORIP=1<CR>
+
+// Configure Domain Name Server
+AT+CDNSCFG="PRIMARY_DNS","SECONDARY_DNS"<CR>
+
+// Connect to Server Example
+AT+CIPSTART="TCP","www.google.com","80"<CR>
+*/
 
 //FONA PINOUT:
 //int VioPin=X; //determines I/O voltage, set to VCC of arduino
@@ -23,15 +38,16 @@ int vibratorPin=3;
 
 String rx=""; //placeholder for string to integer conversion
 unsigned long statusTimer=0; //for telling what state the fona board is in
+unsigned long startupTime;
 fonaStates status=off; //startup state is off.. probably..
 
 void setup()
 {
-	//while(!Serial);
+	while(!Serial);
 
 	Serial.begin(115200);
 	Serial1.begin(19200);
-	Serial1.setTimeout(5000);
+	Serial1.setTimeout(3000);
 
 	Serial.println("power up.");
 
@@ -43,6 +59,8 @@ void setup()
 	pinMode(KeyPin,OUTPUT);
 	pinMode(ResetPin,OUTPUT);
 	pinMode(vibratorPin,OUTPUT);
+
+	buzz(500);
 
 	if(!digitalRead(PSPin)) //if FONA is off, turn it on.
 	{
@@ -62,43 +80,90 @@ void setup()
 	while(status!=connected) ShowSerialData();
 	Serial.println("Connected!");
 
-	delay(3000);
-	ShowSerialData();
-	ShowSerialData();
-	ShowSerialData();
+	delay(2500);
 
-	Serial1.println("at+cstt=\"internet\""); //configure GPRS
-	delay(1000);
-	Serial.println();
-	ShowSerialData();
+	Serial1.println("AT+CIPSTATUS");
 
-	Serial1.println("at+ciicr"); //bring up GPRS connection
-	delay(1000);
-	Serial.println();
-	ShowSerialData();
-	Serial.println();
+	//while(1) ShowSerialData();
 
-	Serial.println("Awaiting Active GPRS connection.");
-	while(status!=gprsActive) ShowSerialData();
-	delay(1000);
+	if(!Serial1.find("STATE: CONNECT OK")) {
+		Serial1.println("at+cstt=\"internet\""); //configure GPRS
+		delay(4000);
+		ShowSerialData();
+		ShowSerialData();
+		ShowSerialData();
 
-	Serial1.println("at+cifsr"); //get IP address
-	delay(1000);
-	Serial.print("IPv4: ");
-	ShowSerialData();
+		Serial.println("Asking for GPRS connection.");
+
+		Serial1.println("at+ciicr"); //bring up GPRS connection
+		delay(3000);
+		while(!Serial1.find("OK")) {
+			Serial.println("Failed to start GPRS connection.. retrying..");
+			Serial1.println("at+ciicr");
+		}
+		ShowSerialData();
+		Serial.println("Awaiting active GPRS connection.");
+		while(status!=gprsActive) ShowSerialData();
+		Serial.println("GPRS connection active!");
+		delay(1000);
+
+		Serial1.println("at+cifsr"); //get IP address
+		delay(1000);
+		Serial.print("IPv4: ");
+		ShowSerialData();
+
+		/*
+		Serial1.println("AT+CIPSTATUS");
+		delay(1000);
+
+		while(1){
+			if(Serial1.available()) Serial.write(Serial1.read());
+			if(Serial.available()) Serial1.write(Serial.read());
+		}
+		*/
+
+		}
+
+  Serial1.println("AT+CIPSTATUS");
+
+  if(!Serial1.find("CONNECT OK")) {
+		while(openTCPconn())
+			{
+				Serial.println("attempt to establish TCP connection failed. Retrying in 5 seconds...");
+				Serial.print("Fona responded with: ");
+				ShowSerialData();
+			}
+		}
+
+	Serial.println("Connected to robottobox!");
+	status=TCPconnected;
+	startupTime=millis();
 }
 
 
 
 void loop()
 {
-	int likes=getLikes();
-	for(int i=0;i<likes;i++) buzz(1000); //one buzz per like
-	Serial.print("Notifications: ");
-	Serial.println(likes);
-    delay(300000); //5 minutes
+	if(millis()>startupTime+60000) {//for every 60 seconds
+		Serial1.println("AT+CIPSTATUS");
+		if(!Serial1.find("CONNECT OK")) {
+			while(openTCPconn()) {
+				Serial.println("attempt to establish TCP connection failed. Retrying in 5 seconds...");
+				}
+			}
+		}
+
+	if(Serial1.available()) {
+		int likes=getLikes();
+		for(int i=0;i<likes;i++) buzz(1000); //one buzz per like
+		Serial.print("Notifications: ");
+		Serial.println(likes);
+		}
+    //delay(300000); //5 minutes
+    //delay(15000); //wait 15 seconds to check if conneciton is still on.
 
 }
+
 void buzz(int duration)
 {
 	digitalWrite(vibratorPin,HIGH);
@@ -111,38 +176,53 @@ void ShowSerialData()
 {
   while(Serial1.available()!=0)
   {
-    delay(50); //seems to fit the timing of the FONA board nicely
+    //delay(90); //seems to fit the timing of the FONA board nicely
+    delay(20);
     Serial.write(Serial1.read());
   }
 }
 
-int getLikes(void)
+void closeTCPconn(void)
+{
+	Serial1.println("at+cipclose");
+}
+
+int openTCPconn()
 {
 	Serial1.println("at+cipstart=\"tcp\",\"62.212.66.171\",\"31337\""); //init conn
-	delay(1000);
+	delay(1500);
+	//while(1) ShowSerialData();
+	if(!Serial1.find("CONNECT OK")) {
+		//closeTCPconn();
+		return 1;
+	}
 	ShowSerialData();
-	Serial1.println("at+cipsend"); //send some data
+	return 0;
+}
+
+int getLikes(void)
+{
+	/*Serial1.println("at+cipsend"); //send some data
 	delay(250);
 	ShowSerialData();
+
 	Serial1.print("yes hello.");
+	delay(250);
 	ShowSerialData();
 	Serial1.println((char)26);
+	delay(250);
 	ShowSerialData();
 	delay(2500);
-	
+	*/
 	//HERE's where the magic happens:
-	while(!Serial1.available());
+	String rx="";
 
-	int inChar=Serial1.read();
-	String rx = ""+(char)inChar;
+	rx += (char)Serial1.read();
+	Serial.print("RX!: ");
+	Serial.print(rx);
 	int likes = rx.toInt();
 
 	//End of magic
-	Serial1.println("at+cipclose");
-
-	delay(500);
-	ShowSerialData();
-	ShowSerialData();
 
 	return likes;
 
--- server/bukseryster.py
+++ server/bukseryster.py
@@ -8,36 +8,36 @@ from IPy import IP
 import sys
 
 if len(sys.argv) == 1:
-	print "Please enter a facebook notifications RSS feed url as argument to this program.. "
-	print "Usage: bukseryster.py <url>"
-	sys.exit(1)
+    print "Please enter a facebook notifications RSS feed url as argument to this program.. "
+    print "Usage: bukseryster.py <url>"
+    sys.exit(1)
 
 feedUrl=sys.argv[1]
 
 #get the feed and parse it:
 def getNotifications(url):
-	notifications=[]
-	try:
-		rawFeed=urllib2.urlopen(url)
-		parsedFeed=feedparser.parse(rawFeed)
+    notifications=[]
+    try:
+        rawFeed=urllib2.urlopen(url)
+        parsedFeed=feedparser.parse(rawFeed)
 
-		#sort notifications into a set:
-		for i in range(len(parsedFeed.entries)):
-			notifications.append(parsedFeed.entries[i].published)
-			#notifications.append(parsedFeed.entries[i].title)
+        #sort notifications into a set:
+        for i in range(len(parsedFeed.entries)):
+            notifications.append(parsedFeed.entries[i].published)
+            #notifications.append(parsedFeed.entries[i].title)
 
-	except Exception as e:
-		print "hmm.. It looks like there was a network timeout: " + str(e)
+    except Exception as e:
+        print "hmm.. It looks like there was a network timeout: " + str(e)
 
-	return notifications
+    return notifications
 
 #create timestamps from notification dates:
 def getStamps(notifications):
-	stamps=[]
-	for i in range(len(notifications)):
-									#rfc2822#section-3.3 format: "Tue, 26 May 2015 15:58:39 -0100"
-		stamps.append(time.mktime(time.strptime(notifications[i], "%a, %d %b %Y %H:%M:%S -0100")))
-	return stamps
+    stamps=[]
+    for i in range(len(notifications)):
+                                    #rfc2822#section-3.3 format: "Tue, 26 May 2015 15:58:39 -0100"
+        stamps.append(time.mktime(time.strptime(notifications[i], "%a, %d %b %Y %H:%M:%S -0100")))
+    return stamps
 
 #STARTUP:
 
@@ -48,6 +48,7 @@ TCP_PORT = 31337
 BUFFER_SIZE = 20  # Normally 1024, but we want fast response
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #make socket reuseable, for debugging (enables you to rerun the program before the socket has timed out)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
 
@@ -62,10 +63,8 @@ while True:
         conn, addr = s.accept()
         print time.ctime(),'Connection from:', addr
         while 1:
-            data = conn.recv(BUFFER_SIZE)
-            if not data: break
-            print "RX:", data
-            print "poking facebook..."
+
+            print time.ctime(),'Poking facebook...'
             newTimeStamps=getStamps(getNotifications(feedUrl))
             diff=[]
             if len(newTimeStamps)>0:
@@ -74,9 +73,10 @@ while True:
             newNotifications+=len(diff)
             #lastTime=time.time()
             print "New notifications:", newNotifications
-            conn.send(str(newNotifications))  # echo
+            conn.send(str(newNotifications))#+'\n')  # echo
             print 'TX:',newNotifications
             newNotifications=0
+            time.sleep(30)
 
         print 'Client disconnected.'
         print '--------------------------'
